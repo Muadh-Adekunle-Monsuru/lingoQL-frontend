f021a9325c96c3c88c6b2e55a790bf524b66fb1b
Muadh Adekunle Monsuru <muadh117a@gmail.com>
2023-11-24 11:27:37 +0100

    changes
    Added material ui
    added landing page
    updated landing page

--- src/Routing/Routing.jsx
+++ src/Routing/Routing.jsx
@@ -1,24 +1,27 @@
-import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-import Chats from "../Pages/Chats";
-import CSV from "../Pages/CSV";
-import NotFound from "../Pages/NotFound";
-import Upload from "../Pages/Upload";
-import URI from "../Pages/URI";
-import Signup from "../Pages/Signup";
-import Login from "../Pages/Login";
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Chats from '../Pages/Chats';
+import CSV from '../Pages/CSV';
+import NotFound from '../Pages/NotFound';
+import Upload from '../Pages/Upload';
+import URI from '../Pages/URI';
+import Signup from '../Pages/Signup';
+import Login from '../Pages/Login';
+import Landing from '../Pages/Landing';
 const Routing = () => {
-  return (
-    <Routes>
-      <Route path="home" element={<Navigate to="/" />} />
-      <Route path="/" element={<Chats />} />
-      <Route path="upload/csv" element={<CSV />} />
-      <Route path="404" element={<NotFound />} />
-      <Route path="upload" element={<Upload />} />
-      <Route path="upload/database" element={<URI />} />
-      <Route path="signup" element={<Signup />} />
-      <Route path="login" element={<Login />} />
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route path='home' element={<Navigate to='/' />} />
+			<Route path='/' element={<Chats />} />
+			<Route path='upload/csv' element={<CSV />} />
+			<Route path='404' element={<NotFound />} />
+			<Route path='*' element={<NotFound />} />
+			<Route path='upload' element={<Upload />} />
+			<Route path='upload/database' element={<URI />} />
+			<Route path='signup' element={<Signup />} />
+			<Route path='login' element={<Login />} />
+			<Route path='landing' element={<Landing />} />
+		</Routes>
+	);
 };
 export default Routing;
