d4b498d473e6460442345817187214411a0c054c
Muadh Adekunle Monsuru <muadh117a@gmail.com>
2023-11-24 12:40:22 +0100

    re-styling of login page

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,72 +1,95 @@
-import React, { useState } from "react";
-import { FcGoogle } from "react-icons/fc";
-import "./Pages.css";
-import PI from "../Components/PageIndicator/PI";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { FcGoogle } from 'react-icons/fc';
+import './Pages.css';
+import PI from '../Components/PageIndicator/PI';
+import { Link } from 'react-router-dom';
+import Paper from '@mui/material/Paper';
+import { Button } from '@mui/material';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
 
 const Signup = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  console.log(name, email, password);
-  return (
-    <PI title="signup">
-      <section className="login--wrapper">
-        <div className="login--content">
-          <div className="login">
-            <h3 className="login--title">Sign up</h3>
-          </div>
-          <br />
-          <button className="general--btn">
-            <FcGoogle />
-            Continue with Google
-          </button>
-          <br />
-          <br />
-          <p style={{ fontSize: 17, fontWeight: 500 }}>or</p>
-          <form className="login--form form">
-            <div className="entry--container">
-              <input
-                type="text"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                name="name"
-                placeholder="Full Name"
-              />
-            </div>
-            <div className="entry--container">
-              <input
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                name="email"
-                placeholder="Email"
-              />
-            </div>
-            <div className="entry--container">
-              <input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Password"
-              />
-            </div>
-            <br />
-            <button type="submit" className="general--btn">
-              <Link className="general--btn">Sign up</Link>
-            </button>
-            <br />
-            <p className="signup-writeup">
-              Already have an account?
-              <Link className="signup" to="/login">
-                Log in
-              </Link>
-            </p>
-          </form>
-        </div>
-      </section>
-    </PI>
-  );
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	console.log(name, email, password);
+	return (
+		<PI title='signup'>
+			<Container fixed maxWidth='sm'>
+				<h1 className='login--title'>Sign up</h1>
+				<Paper
+					elevation={5}
+					style={{
+						display: 'grid',
+						padding: 20,
+						justifyContent: 'center',
+					}}
+				>
+					<Button variant='outlined' startIcon={<FcGoogle />}>
+						Continue with Google
+					</Button>
+					<p style={{ fontSize: 12, fontWeight: 500, textAlign: 'center' }}>
+						or
+					</p>
+					<p style={{ fontSize: 17, fontWeight: 500, textAlign: 'center' }}>
+						Sign up with Email
+					</p>
+					<Box
+						component='form'
+						sx={{
+							'& > :not(style)': { m: 1, width: '25ch' },
+						}}
+						noValidate
+						autoComplete='off'
+						style={{
+							justifyContent: 'center',
+							alignItems: 'center',
+							display: 'grid',
+						}}
+					>
+						<TextField
+							id='outlined-basic'
+							label='Full Name'
+							variant='outlined'
+							value={name}
+							onChange={(e) => setName(e.target.value)}
+							required
+						/>
+						<TextField
+							id='outlined-basic'
+							label='Email'
+							variant='outlined'
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+							required
+						/>
+						<TextField
+							id='outlined-basic'
+							label='Password'
+							variant='outlined'
+							type='password'
+							autoComplete='current-password'
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+							required
+						/>
+					</Box>
+
+					<Button variant='contained' onClick={() => navigate('/signup')}>
+						Register
+					</Button>
+					<p className='signup-writeup' style={{ textAlign: 'center' }}>
+						Already have an account?
+						<Link className='signup' to='/login'>
+							Log in
+						</Link>
+					</p>
+				</Paper>
+			</Container>
+		</PI>
+	);
 };
 
 export default Signup;
