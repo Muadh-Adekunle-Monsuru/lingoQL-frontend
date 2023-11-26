3b958a474a0f665feb54481a314a6e5b0c3ac8dc
TijanAyo <ayomidetijani6@gmail.com>
2023-11-26 22:11:14 +0100

    Removed error filled code

--- src/Components/PageIndicator/PI.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "../PageIndicator/PI.css";
-
-const PI = (props) => {
-  document.title = "LingoQL - " + props.title;
-  return <div className="PI">{props.children}</div>;
-};
-
-export default PI;
--- src/Pages/URI.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const URI = () => {
-  return <div>URI</div>;
-};
-
-export default URI;
