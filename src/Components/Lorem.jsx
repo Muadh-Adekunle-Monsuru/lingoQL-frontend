3a0c1e071951d0a3e55b006b0093dce20f33f20b
Muadh Adekunle Monsuru <muadh117a@gmail.com>
2023-11-27 10:34:56 +0100

    landingpage update

--- src/Components/Lorem.jsx
+++ src/Components/Lorem.jsx
@@ -2,70 +2,26 @@ export function LoremIpsum() {
 	return (
 		<>
 			<article>
-				<div className='p-10 m-5 border-dashed border-2 rounded-lg'>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac
-						rhoncus quam.
-					</p>
+				<div className='p-10 m-5  border-2 rounded-lg hover:gradient-border-btn'>
+					<h1>
+						The challenge is to simplify data interaction for non-tech users in
+						an e-commerce setting.
+					</h1>
 				</div>
 				<div className='p-3 m-5 border-dashed border-2 rounded-lg'>
+					<h2>Target Audience</h2>
 					<p>
-						Fringilla quam urna. Cras turpis elit, euismod eget ligula quis,
-						imperdiet sagittis justo. In viverra fermentum ex ac vestibulum.
-						Aliquam eleifend nunc a luctus porta. Mauris laoreet augue ut felis
-						blandit, at iaculis odio ultrices. Nulla facilisi. Vestibulum cursus
-						ipsum tellus, eu tincidunt neque tincidunt a.
-					</p>
-				</div>
-				<div className='p-3 m-5 border-dashed border-2 rounded-lg'>
-					<h2>Sub-header</h2>
-					<p>
-						In eget sodales arcu, consectetur efficitur metus. Duis efficitur
-						tincidunt odio, sit amet laoreet massa fringilla eu.
-					</p>
-					<p>
-						Pellentesque id lacus pulvinar elit pulvinar pretium ac non urna.
-						Mauris id mauris vel arcu commodo venenatis. Aliquam eu risus arcu.
-						Proin sit amet lacus mollis, semper massa ut, rutrum mi.
-					</p>
-				</div>
-				<div className='p-3 m-5 border-dashed border-2 rounded-lg'>
-					<p>Sed sem nisi, luctus consequat ligula in, congue sodales nisl.</p>
-					<p>
-						Vestibulum bibendum at erat sit amet pulvinar. Pellentesque pharetra
-						leo vitae tristique rutrum. Donec ut volutpat ante, ut suscipit leo.
+						Non-technical users in businesses who want to interact with
+						e-commerce data without the need for technical skills.
 					</p>
 				</div>
 				<div className='p-3 m-5 border-dashed border-2 rounded-lg'>
 					<h2>Sub-header</h2>
 					<p>
-						Maecenas quis elementum nulla, in lacinia nisl. Ut rutrum fringilla
-						aliquet. Pellentesque auctor vehicula malesuada. Aliquam id feugiat
-						sem, sit amet tempor nulla. Quisque fermentum felis faucibus,
-						vehicula metus ac, interdum nibh. Curabitur vitae convallis ligula.
-						Integer ac enim vel felis pharetra laoreet. Interdum et malesuada
-						fames ac ante ipsum primis in faucibus. Pellentesque hendrerit ac
-						augue quis pretium.
-					</p>
-				</div>
-				<div className='p-3 m-5 border-dashed border-2 rounded-lg'>
-					<p>
-						Morbi ut scelerisque nibh. Integer auctor, massa non dictum
-						tristique, elit metus efficitur elit, ac pretium sapien nisl nec
-						ante. In et ex ultricies, mollis mi in, euismod dolor.
-					</p>
-					<p>Quisque convallis ligula non magna efficitur tincidunt.</p>
-					<p>
-						Pellentesque id lacus pulvinar elit pulvinar pretium ac non urna.
-						Mauris id mauris vel arcu commodo venenatis. Aliquam eu risus arcu.
-						Proin sit amet lacus mollis, semper massa ut, rutrum mi.
-					</p>
-				</div>
-				<div className='p-3 m-5 border-dashed border-2 rounded-lg'>
-					<p>Sed sem nisi, luctus consequat ligula in, congue sodales nisl.</p>
-					<p>
-						Vestibulum bibendum at erat sit amet pulvinar. Pellentesque pharetra
-						leo vitae tristique rutrum. Donec ut volutpat ante, ut suscipit leo.
+						Traditional database interfaces are complex and unintuitive.
+						Solution: The chat interface provides a user-friendly platform,
+						making it easy for Alex to interact with the database using simple
+						conversational language.
 					</p>
 				</div>
 			</article>
--- src/Pages/NewLanding.jsx
+++ src/Pages/NewLanding.jsx
@@ -1,19 +1,24 @@
 import React, { useState } from 'react';
 import PI from '../Components/PageIndicator/PI';
-// import './Pages.css';
-// import Grid from '@mui/material/Grid';
-// import { Box } from '@mui/material';
-// import Button from '@mui/material/Button';
-// import Paper from '@mui/material/Paper';
+
+import { motion, useScroll, useSpring } from 'framer-motion';
+
 import { Outlet, Link, useNavigate } from 'react-router-dom';
 import newlogo from '/newlogo.png';
+import { LoremIpsum } from '../Components/Lorem';
 const NewLanding = () => {
-	const [email, setEmail] = useState('');
 	const navigate = useNavigate();
+	const { scrollYProgress } = useScroll();
+	const scaleX = useSpring(scrollYProgress, {
+		stiffness: 100,
+		damping: 30,
+		restDelta: 0.001,
+	});
 	return (
-		<PI title='Home' class='h-vh'>
+		<PI title='Home' className='h-vh'>
+			<motion.div className='progress-bar' style={{ scaleX }} />
 			<div
-				class='flex justify-between items-center  w-full  bg-black px-4 py-2'
+				className='flex justify-between items-center  w-full  bg-black px-4 py-2'
 				id='cwt01doa3o'
 			>
 				<div className='logo'>
@@ -21,35 +26,30 @@ const NewLanding = () => {
 						<img src={newlogo} alt='logo' />
 					</Link>
 				</div>
-				<h2 class='text-2xl font-semibold text-white'>LingoQL</h2>
-				<a class='text-white underline' href='/login'>
+				<h2 className='text-2xl font-semibold text-white'>LingoQL</h2>
+				<a className='text-white underline' href='/login'>
 					Log In
 				</a>
 			</div>
-			<section class='w-full h-full py-12 md:py-24 lg:py-32 xl:py-48 bg-black'>
-				<div class='px-4 md:px-6'>
-					<div class='grid gap-7 items-center justify-center'>
-						<div class='flex flex-col justify-center space-y-4 text-center'>
-							<div class='space-y-2'>
-								<h1 class='text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none bg-clip-text text-transparent bg-gradient-to-r from-white to-gray-500'>
-									Revolutionize Your Query Experience
+			<section className='w-full h-full py-12 md:py-24 lg:py-32 xl:py-48 bg-black'>
+				<div className='px-4 md:px-6'>
+					<div className='grid gap-7 items-center justify-center'>
+						<div className='flex flex-col justify-center space-y-4 text-center'>
+							<div className='space-y-2'>
+								<h1 className=' text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none bg-clip-text text-transparent bg-gradient-to-r from-white to-gray-500'>
+									Revolutionize Your{' '}
+									<span className='gradient-text'>Query </span>
+									Experience
 								</h1>
-								<p class='max-w-[600px] text-zinc-200 md:text-xl dark:text-zinc-100 mx-auto'>
+								<p className='max-w-[600px] text-zinc-200 md:text-xl dark:text-zinc-100 mx-auto'>
 									Join us and take control of your data. Unlock insights
 									seamlessly; bridging the data divide with intuitive querying.
 								</p>
 							</div>
-							<div class='w-full max-w-sm space-y-2 mx-auto '>
-								<form class='flex space-x-2 items-center justify-center'>
-									{/* <input
-									class='flex h-10 w-full rounded-md border px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 max-w-lg flex-1 bg-gray-800 text-white border-gray-900'
-									placeholder='Enter your email'
-									type='email'
-									value={email}
-									onChange={(e) => setEmail(e.target.value)}
-								/> */}
+							<div className='w-full max-w-sm space-y-2 mx-auto '>
+								<form className='flex space-x-2 items-center justify-center'>
 									<button
-										class='inline-flex items-center justify-center my-5 rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-primary/90 h-10 px-4 py-2 bg-white text-black'
+										className=' hover:gradient-border-btn inline-flex items-center justify-center my-5 rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-sky-100 hover:bg-primary/90 h-10 px-4 py-2 bg-white text-black'
 										type='submit'
 										onClick={() => navigate(`/signup`)}
 									>
